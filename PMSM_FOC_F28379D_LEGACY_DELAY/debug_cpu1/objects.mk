1067dec8b90f5c4e3227ea2873c66159c5c63f79
YY <young_yan1996@163.com>
2021-06-05 22:54:23 +0800

    Run bat and delete the debug info!

--- PMSM_FOC_F28379D_LEGACY_DELAY/debug_cpu1/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lrts2800_fpu32_fast_supplement.lib -lIQmath_fpu32.lib -lcla1_math_library_fpu32.lib -lfast_current_loop.lib -llibc.a -llibc.a
-
